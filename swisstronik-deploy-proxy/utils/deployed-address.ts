a1d532b29220bcafba5cd4ccb9ed169dcd1ebab1
phuong1492 <phuong1492.bka@gmail.com>
2024-08-19 11:39:43 +0900

    add swisstronik-deploy-proxy

---
+++ swisstronik-deploy-proxy/utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x2C7B7146c925eB556B2DF361893Fc532d6384A3A'
+export const ProxyAdmin = '0xf5dc4f0442A975e0246Ea8e73D309bce041E7484'
+export const SWTRImplementation = '0x7d58e27F200d8370dC25a0bbC2C8010344FaE88F'
+    
